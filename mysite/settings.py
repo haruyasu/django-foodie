76944210d04821b276bbb8ce136d783be176540c
haruyasu <harukun2002@gmail.com>
2020-03-29 16:37:07 +0900

    :ambulance: heroku設定

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -137,3 +138,5 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 # EMAIL_HOST_USER = 'gmail adress'
 # EMAIL_HOST_PASSWORD = 'gmail password'
 # EMAIL_USE_TLS = True
+
+django_heroku.settings(locals())
